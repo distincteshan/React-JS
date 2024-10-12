05ec3eaef42efa45165b0062733fd5a29d5cb93b
distincteshan <deosatwareshan@gmail.com>
2024-10-13 01:30:38 +0530

    First

---
+++ exercise1/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+
+const Footer = () => {
+  return (
+    <footer>
+        <p>2024 My Website</p>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ exercise1/src/components/MainContent.jsx
@@ -0,0 +1,10 @@
+const MainContent = () => {
+  return (
+    <main>
+        <h2>Main Content</h2>
+        <p>Lionel Messi is the GOAT!</p>
+    </main>
+  )
+}
+
+export default MainContent
